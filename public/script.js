9c097156876843fa6d6822cc8497a15d37ccdc8c
hayate_y <hayate.bgu.senyo@gmail.com>
2022-07-20 00:59:50 +0900

    [Add] クライアントサイドからデータ作成

--- public/script.js
+++ public/script.js
@@ -1,4 +1,6 @@
-const tasksDOM = document.querySelector(".tasks")
+const tasksDOM = document.querySelector(".tasks");
+const formDOM = document.querySelector(".task-form");
+const taskInputDOM = document.querySelector(".task-input");
 
 // /api/v1/tasksからタスクを読み込む
 const showTasks = async () => {
@@ -8,10 +10,11 @@ const showTasks = async () => {
     console.log(tasks);
 
     // タスクを出力
-    const allTasks = tasks.map((task) => {
-      const { completed, _id, name } = task;
-      
-      return `<div class="single-task">
+    const allTasks = tasks
+      .map((task) => {
+        const { completed, _id, name } = task;
+
+        return `<div class="single-task">
       <h5><span><i class="far fa-check-circle"></i></span>${name}</h5>
       <div class="task-links">
         <!-- 編集リンク -->
@@ -23,13 +26,27 @@ const showTasks = async () => {
           <i class="fas fa-trash"></i>
         </button>
       </div>
-    </div>`
-  }).join("");
-  console.log({allTasks});
-  tasksDOM.innerHTML = allTasks
+    </div>`;
+      })
+      .join("");
+    console.log(allTasks);
+    tasksDOM.innerHTML = allTasks;
   } catch (err) {
     console.log(err);
   }
 };
 
 showTasks();
+
+// タスクを新規作成する
+formDOM.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const name = taskInputDOM.value;
+  try {
+    await axios.post("/api/v1/tasks", { name: name });
+    showTasks();
+    taskInputDOM.value = "";
+  } catch (err) {
+    console.log(err);
+  }
+});
