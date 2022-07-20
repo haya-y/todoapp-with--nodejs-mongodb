7a1ece216abaa233ab3c55cdf0ec1d9d59c48010
hayate_y <hayate.bgu.senyo@gmail.com>
2022-07-20 21:14:50 +0900

    [Add] Delete機能・エラーハンドリングの実装

--- public/script.js
+++ public/script.js
@@ -1,13 +1,19 @@
 const tasksDOM = document.querySelector(".tasks");
 const formDOM = document.querySelector(".task-form");
 const taskInputDOM = document.querySelector(".task-input");
+const formAlertDOM = document.querySelector(".form-alert");
 
 // /api/v1/tasksからタスクを読み込む
 const showTasks = async () => {
   try {
     // 自作のAPIを叩く
     const { data: tasks } = await axios.get("/api/v1/tasks");
-    console.log(tasks);
+
+    // タスクが1つもないとき
+    if (tasks.length < 1) {
+      tasksDOM.innerHTML = `<h5 class="empty-list">タスクがありません</h5>`;
+      return;
+    }
 
     // タスクを出力
     const allTasks = tasks
@@ -22,14 +28,13 @@ const showTasks = async () => {
           <i class="fas fa-edit"></i>
         </a>
         <!-- ゴミ箱リンク -->
-        <button type="button" class="delete-btn">
+        <button type="button" class="delete-btn" data-id=${_id}>
           <i class="fas fa-trash"></i>
         </button>
       </div>
     </div>`;
       })
       .join("");
-    console.log(allTasks);
     tasksDOM.innerHTML = allTasks;
   } catch (err) {
     console.log(err);
@@ -46,7 +51,30 @@ formDOM.addEventListener("submit", async (event) => {
     await axios.post("/api/v1/tasks", { name: name });
     showTasks();
     taskInputDOM.value = "";
+    formAlertDOM.style.display = "block";
+    formAlertDOM.textContent = "タスクを追加しました。";
+    formAlertDOM.classList.add("text-success");
   } catch (err) {
     console.log(err);
+    formAlertDOM.style.display = "block";
+    formAlertDOM.classList.remove("text-success");
+    formAlertDOM.innerHTML = "無効です。もう一度やり直してください。";
+  }
+  setTimeout(() => {
+    formAlertDOM.style.display = "none";
+  }, 3000);
+});
+
+// タスクを削除する
+tasksDOM.addEventListener("click", async (event) => {
+  const element = event.target;
+  if (element.parentElement.classList.contains("delete-btn")) {
+    try {
+      const id = element.parentElement.dataset.id;
+      await axios.delete(`/api/v1/tasks/${id}`);
+      showTasks();
+    } catch (err) {
+      console.log(err);
+    }
   }
 });
