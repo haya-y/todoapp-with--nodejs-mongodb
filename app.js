db85dc86dd83398160a7c4fbc9ab7eb4b7e0c48f
hayate_y <hayate.bgu.senyo@gmail.com>
2022-07-21 20:25:11 +0900

    [Add] デプロイ用のPortの追加

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use("/api/v1/tasks", taskRoute);
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URL);
-    app.listen(PORT, console.log("server is set up."));
+    app.listen(process.env.PORT || PORT, console.log("server is set up."));
   } catch (err) {
     console.log(err);
   }
